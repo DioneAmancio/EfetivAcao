0a0a3dcc4042ab59a5c49eab9e3e5e5cc3934c78
dione_amancio <fcodione@gmail.com>
2017-02-14 09:55:14 -0300

    Melhorias

--- date.py
+++ date.py
@@ -3,26 +3,30 @@ from datetime import timedelta
 from datetime import date
 
 
+def feriadosMoveis():
+    print("oi")
+
 def feriadosFixos():
     ano = date.today ()
     ano = str (ano.year)
     listaFeriados = []
+    #feriadosNacionais = ('01/01/ 21/04/ 01/01/ 07/07/ 12/10/ 02/11/ 15/11/ 25/12/ )
+    #feriadosEstaduaisEMunicipais = ('19/03/ 25/03/ 15/08/ ')
     feriados = ('01/01/ 21/04/ 01/01/ 07/07/ 12/10/ 02/11/ 15/11/ 25/12/ 19/03/ 25/03/ 15/08/').split (' ')
-    # feriadosEstaduaisEMunicipais = ('19/03/ 25/03/ 15/08/ ')
     for i in range (len (feriados)):
         data = feriados[i]
         data += ano
         listaFeriados.append (datetime.strptime (data, '%d/%m/%Y').date ())
     return listaFeriados
 
-
-def calcularPrazos(diasUteis):
-    dataFimSP = datetime.strptime (parametro, '%d/%m/%Y').date ()
+def calcularPrazos(dataFim, diasUteis):
+    dataFimSP = datetime.strptime (dataFim, '%d/%m/%Y').date ()
     prazoSubmeter = dataFimSP
     feriado = feriadosFixos ()
-    while (diasUteis != 7):
+    prazo = 0
+    while (prazo != diasUteis):
         if ((prazoSubmeter.weekday () != 5 and prazoSubmeter.weekday () != 6) and (prazoSubmeter not in feriado)):
-            diasUteis = diasUteis + 1
+            prazo = prazo + 1
         prazoSubmeter += timedelta (days=1)
     print (prazoSubmeter)
 
